e1c8be6290a2bece097405715694436dc1502521
Roni Braga <sousaronielson@gmail.com>
2024-08-20 22:02:34 -0300

    adicionando o componente logo. logotipo e landingpage

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,11 +1,49 @@
 import React from 'react';
-import { MenuNav } from '../../components/menunav/MenuNav';
+import { StyleContainer,StyleLogoTitulo,StyleForm,StyleFormOne,StyleFormTwo,StyleFormUser,StyleButton,StyleInput,StyleH1,StyleContainerCep, StyleInputPersonalizadoCep,StyleInputPersonalizadoNumero,StylesAll  } from '../../components/cadastro/FormUserStyle';
+import { Logo, } from '../../components/cadastro/Logo';
 
 export const LandingPage = () => {
    
     return (
     <>
-        <MenuNav/>
+    <StyleContainer>
+         <StyleLogoTitulo>              
+            <Logo/>          
+           <StyleH1>TECH READER</StyleH1>
+        </StyleLogoTitulo>
+     <StyleForm>
+        <StyleFormOne>
+        <StyleFormTwo>
+            <StyleFormUser>
+                <label>Nome</label>
+                <StyleInput></StyleInput>
+                <label>Email</label>
+                <StyleInput></StyleInput>
+                <label>Senha</label>
+                <StyleInput></StyleInput>
+                <label>Confirmar Senha</label>
+                <StyleInput></StyleInput>
+                <StylesAll>    
+                <StyleInputPersonalizadoCep>
+                <label>CEP</label> 
+                <StyleContainerCep></StyleContainerCep>          
+                </StyleInputPersonalizadoCep> 
+                <StyleInputPersonalizadoNumero>
+                <label>Número</label> 
+                <StyleContainerCep></StyleContainerCep>
+                </StyleInputPersonalizadoNumero>          
+                </StylesAll>             
+                <label>Rua</label>
+                <StyleInput></StyleInput>
+                <label>Bairro</label>
+                <StyleInput></StyleInput>
+                <StyleButton>Cadastrar</StyleButton>
+            </StyleFormUser>
+        </StyleFormTwo>
+        </StyleFormOne>       
+     </StyleForm>
+    </StyleContainer>
+        
     </>
 )
 }
