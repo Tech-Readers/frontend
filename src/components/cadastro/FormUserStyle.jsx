e78ae1a476df2cccac93ed6e2162b852b162c5f1
Roni Braga <sousaronielson@gmail.com>
2024-08-20 21:57:57 -0300

    projeto form de cadastro do ususario

---
+++ src/components/cadastro/FormUserStyle.jsx
@@ -0,0 +1,104 @@
+import styled from "styled-components";
+
+export const StyleContainer = styled.div`
+    background-color: #E8E8E8;
+    width: 100vw;
+    height: 100vh;
+    display:flex;
+    justify-content: space-around;
+    overflow:hidden;
+`
+export const StyleLogoTitulo = styled.div`
+    width: 735px;
+    font-size: 20px;
+    display:flex;
+    align-items:center;
+    flex-direction:column;
+    justify-content:center;
+    text-align:center;
+        
+`
+export const StyleH1 = styled.h1`
+    width: 393px;
+    height: 249px;
+    font-size: 80px;
+    top:500px;
+    padding-bottom:30px;
+` 
+export const StyleForm = styled.div`
+     width: 635px;
+     padding-left: 7px;    
+     background-color: #B2A397;
+   
+`
+export const StyleFormOne = styled.div`
+     width: 635px;
+     height:100vh;
+     padding-left: 10px;
+     background-color: #C8B4A6;
+      
+`
+export const StyleFormTwo = styled.div`
+     width: 635px;
+     height: 100vh;
+     left: 732px;
+     background-color: #D7C4B6;
+    display: flex;    
+    align-items:center;
+    justify-content:center;
+    
+    
+    
+`
+export const StyleFormUser = styled.form`
+     display:flex;
+     flex-direction:column;
+     width: 412px;
+     height: 560px;
+     border-radius: 23px;
+     background-color:#EEEEEE;
+     opacity:75%;
+     align-items:center;
+     justify-content:center;
+     box-shadow: 0 0 8px;
+     
+
+`
+export const StyleButton = styled.button`
+    width: 300px;
+    height: 38px;
+    border-radius: 10px;
+    background-color: #5A645F;
+    color: #E8E8E8;
+    font-size: 18px;
+    font-family: Poppins;
+    margin-top:10px;
+
+`
+export const StyleInput = styled.input`
+    width: 314px;
+    height: 36px;
+    border-radius: 10px;
+    margin-bottom:5px;
+`
+export const StylesAll = styled.div`
+    display:flex;
+   
+
+`
+export const StyleInputPersonalizadoCep = styled.div`
+    display:flex;
+    flex-direction:column;
+    
+    
+`
+export const StyleInputPersonalizadoNumero = styled.div`
+    display:flex;
+    flex-direction:column;
+`
+export const StyleContainerCep = styled.input`
+    width: 150px;
+    height: 30px;
+    border-radius: 10px;
+    margin-right:5px;
+`
