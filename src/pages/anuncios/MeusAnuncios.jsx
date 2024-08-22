3e4f7cc830da533625df45336efe3088e78204e0
FrancisLauriano <130801505+FrancisLauriano@users.noreply.github.com>
2024-08-22 19:38:51 -0300

    .

--- src/pages/anuncios/MeusAnuncios.jsx
+++ src/pages/anuncios/MeusAnuncios.jsx
@@ -1,15 +1,15 @@
 //src/pages/anuncios/MeusAnuncios.jsx:
 import React, { useEffect, useState } from 'react';
-import { useHistory } from 'react-router-dom';
 import { getExchangesByUserId, deleteExchange, closeExchange } from '../../services/exchangeService';
 import AdCard from '../../components/anuncios/AdCard';
+import AtualizarAnuncio from './AtualizarAnuncio';
 import Footer from '../../components/footer/footer';
 import { Container, AdsContainer, Header } from './MeusAnuncios.styled';
 
 const MeusAnuncios = () => {
   const [ads, setAds] = useState([]);
   const [selectedAd, setSelectedAd] = useState(null);
-  const history = useHistory();
+  const [isEditing, setIsEditing] = useState(false);
 
   useEffect(() => {
     const fetchAds = async () => {
@@ -30,9 +30,9 @@ const MeusAnuncios = () => {
 
   const handleDeleteAd = async (id) => {
     try {
-      await deleteExchange(id); // exclui anuncio
+      await deleteExchange(id);
       setAds(ads.filter(ad => ad.id !== id));
-      setSelectedAd(null);  // Limpa a seleção se o anúncio selecionado for excluído
+      setSelectedAd(null);
     } catch (error) {
       console.error('Erro ao excluir o anúncio:', error);
     }
@@ -41,15 +41,21 @@ const MeusAnuncios = () => {
   const handleToggleAd = async (id) => {
     try {
       await closeExchange(id);
-      setSelectedAd(prev => ({ ...prev, ativo: false }));  // Marca o anúncio como inativo
+      setSelectedAd(prev => ({ ...prev, ativo: false }));
     } catch (error) {
       console.error('Erro ao desativar o anúncio:', error);
     }
   };
 
   // atualizar anuncio
-  const handleEditAd = (id) => {
-    history.push(`/anuncios/atualizar/${id}`);
+  const handleEditAd = () => {
+    setIsEditing(true);
+  };
+
+  const handleUpdateSuccess = () => {
+    setIsEditing(false);
+    setSelectedAd(null);
+    // Atualizar a lista de anúncios após a edição, se necessário
   };
 
   return (
@@ -58,51 +64,60 @@ const MeusAnuncios = () => {
         <h1>Meus Anúncios</h1>
       </Header>
 
-      {selectedAd && (
-        <AdCard
-          key={selectedAd.id}
-          id={selectedAd.id}
-          title={selectedAd.titulo}
-          genre={selectedAd.genero_livro_oferecido}
-          author={selectedAd.autor_livro_oferecido}
-          description={selectedAd.descricao}
-          profileImageUrl={selectedAd.profileImageUrl}
-          bookImageUrl={selectedAd.bookImageUrl}
-          isSelected={true}
-          onSelect={() => setSelectedAd(null)}
-          onToggle={() => handleToggleAd(selectedAd.id)}
-          onEdit={() => handleEditAd(selectedAd.id)}
-          onDelete={() => handleDeleteAd(selectedAd.id)}
-          isActive={selectedAd.ativo}
-        />
-      )}
-
-      <AdsContainer>
-        {ads
-          .filter(ad => !selectedAd || ad.id !== selectedAd.id)  // Filtra o anúncio selecionado
-          .map(ad => (
+      {isEditing && selectedAd ? (
+        <AtualizarAnuncio id={selectedAd.id} onSuccess={handleUpdateSuccess} />
+      ) : (
+        <>
+          {selectedAd && (
             <AdCard
-              key={ad.id}
-              id={ad.id}
-              title={ad.titulo}
-              genre={ad.genero_livro_oferecido}
-              author={ad.autor_livro_oferecido}
-              description={ad.descricao}
-              profileImageUrl={ad.profileImageUrl}
-              bookImageUrl={ad.bookImageUrl}
-              isSelected={false}
-              onSelect={() => handleSelectAd(ad)}
-              onToggle={() => handleToggleAd(ad.id)}
-              isActive={ad.ativo}
+              key={selectedAd.id}
+              id={selectedAd.id}
+              title={selectedAd.titulo}
+              genre={selectedAd.genero_livro_oferecido}
+              author={selectedAd.autor_livro_oferecido}
+              description={selectedAd.descricao}
+              profileImageUrl={selectedAd.profileImageUrl}
+              bookImageUrl={selectedAd.bookImageUrl}
+              isSelected={true}
+              onSelect={() => setSelectedAd(null)}
+              onToggle={() => handleToggleAd(selectedAd.id)}
+              onEdit={handleEditAd}
+              onDelete={() => handleDeleteAd(selectedAd.id)}
+              isActive={selectedAd.ativo}
             />
-          ))}
-      </AdsContainer>
+          )}
+
+          <AdsContainer>
+            {ads
+              .filter(ad => !selectedAd || ad.id !== selectedAd.id)  // filtra o anúncio selecionado
+              .map(ad => (
+                <AdCard
+                  key={ad.id}
+                  id={ad.id}
+                  title={ad.titulo}
+                  genre={ad.genero_livro_oferecido}
+                  author={ad.autor_livro_oferecido}
+                  description={ad.descricao}
+                  profileImageUrl={ad.profileImageUrl}
+                  bookImageUrl={ad.bookImageUrl}
+                  isSelected={false}
+                  onSelect={() => handleSelectAd(ad)}
+                  onToggle={() => handleToggleAd(ad.id)}
+                  isActive={ad.ativo}
+                />
+              ))}
+          </AdsContainer>
+        </>
+      )}
       <Footer />
     </Container>
   );
 };
 
 export default MeusAnuncios;
+
+
+
 
 
 
