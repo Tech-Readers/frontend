5ec7938adbcd563579d1238dcc2812d3b86b3aa7
metioflar <nicollelms@gmail.com>
2024-08-23 00:13:11 -0300

    bugfix: global style

--- src/GlobalStyle.jsx
+++ src/GlobalStyle.jsx
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 import React from "react";
 import styled from "styled-components";
 import "./assets/fonts.css";
-=======
-// eslint-disable-next-line no-unused-vars
-import React from 'react';
->>>>>>> origin/home-thais
 
 export const Theme = {
     colors: {
