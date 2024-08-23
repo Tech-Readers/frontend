db11b86fc208b2a3af17598d3c9b092422b4541e
tiagoolima <tiagotome097@gmail.com>
2024-08-23 18:55:48 -0300

    Criação da responsividade

--- src/pages/Profile/Style.jsx
+++ src/pages/Profile/Style.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Body = styled.div`
     display: flex;
     flex-direction: column;
-    width: 100vw;
+    width: 100%;
     box-sizing: border-box;
     padding: 0;
     margin: 0;
@@ -11,10 +11,19 @@ export const Body = styled.div`
     font-size: 1rem;
     background-color: ${(props) => props.theme.colors.Branco};
     min-height: 100vh;
+    padding-top: 5rem; 
+
+    @media (max-width: 768px) {
+        padding-top: 6rem; 
+    }
 `;
 
 export const ImagemLogo = styled.img`
     width: 10rem;
+
+    @media (max-width: 768px) {
+        width: 7rem;
+    }
 `;
 
 export const Navigation = styled.div`
@@ -22,7 +31,7 @@ export const Navigation = styled.div`
     color: ${(props) => props.theme.colors.VerdeEscuro};
     width: 100vw;
     height: 5rem;
-    position: relative;
+    position: fixed;
     top: 0;
     z-index: 10;
     display: flex;
@@ -32,6 +41,12 @@ export const Navigation = styled.div`
     justify-content: space-evenly;
     padding-top: 0.3rem;
     box-shadow: 0 4px 4px rgba(0, 0, 0, 0.1);
+
+    @media (max-width: 768px) {
+        flex-direction: column;
+        height: auto;
+        padding: 1rem;
+    }
 `;
 
 export const ListStyle = styled.ul`
@@ -50,6 +65,16 @@ export const ListStyle = styled.ul`
             text-decoration: none;
         }
     }
+
+    @media (max-width: 768px) {
+        flex-direction: column;
+        margin-left: 0;
+        margin-top: 1rem;
+
+        li {
+            margin: 0.5rem 0;
+        }
+    }
 `;
 
 export const ProfileNavegation = styled.img`
@@ -57,20 +82,31 @@ export const ProfileNavegation = styled.img`
     height: 3rem;
     border-radius: 50%;
     margin-right: 2px;
+
+    @media (max-width: 768px) {
+        width: 2.5rem;
+        height: 2.5rem;
     }
-`
+`;
 
 export const LoginContainer = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    width: 80%;
-    margin-top: 6rem;
+    width: 90%;  
+    margin-top: 8rem; 
     padding: 2rem;
     background-color: ${(props) => props.theme.colors.CinzaUm};
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
     border-radius: 8px;
-    flex-direction: row; /* Coloca os elementos em linha */
+    flex-direction: row; 
+
+    @media (max-width: 768px) {
+        flex-direction: column;
+        width: 95%;
+        padding: 1rem;
+        margin-top: 10rem; 
+    }
 `;
 
 export const ProfilePic = styled.div`
@@ -78,10 +114,18 @@ export const ProfilePic = styled.div`
     height: 16rem;
     border-radius: 50%;
     overflow: hidden;
-    margin-right: 2rem; /* Adiciona espaço à direita da imagem */
+    margin-right: 2rem; 
+
     img {
         width: 100%;
         height: 100%;
+    }
+
+    @media (max-width: 768px) {
+        width: 12rem;
+        height: 12rem;
+        margin-right: 0;
+        margin-bottom: 1rem;
     }
 `;
 
@@ -90,7 +134,6 @@ export const UserInfo = styled.div`
     justify-content: center;
     align-items: center;
     gap: 4.6rem; 
-    
 
     div {
         display: flex;
@@ -107,22 +150,33 @@ export const UserInfo = styled.div`
         margin: 1.5rem 0;
         padding: 0.7rem 1.5rem;
         background-color: ${(props) => props.theme.colors.CinzaUm};
-        color: color: ${(props) => props.theme.colors.Preto};
+        color: ${(props) => props.theme.colors.Preto};
         border: 1px solid black;
         border-radius: 10px;
         cursor: pointer;
         font-size: 1rem;
     }
 
-    .bio{
+    .bio {
         width: 30rem;
         margin: 3rem;
         font-size: 1.2rem;
     }
+
+    @media (max-width: 768px) {
+        flex-direction: column;
+        gap: 1.5rem;
+
+        .bio {
+            width: 75%;
+            margin: 1rem 0;
+            font-size: 1rem;
+        }
+    }
 `;
 
 export const AdSection = styled.section`
-    width: 80%;
+    width: 90%;  
     margin: 2rem 0;
 
     h2 {
@@ -134,6 +188,17 @@ export const AdSection = styled.section`
     div {
         display: flex;
         justify-content: space-between;
+        flex-wrap: wrap;
+    }
+
+    @media (max-width: 768px) {
+        width: 75%;
+        margin: 1rem 0;
+
+        div {
+            flex-direction: column;
+            align-items: center;
+        }
     }
 `;
 
@@ -172,12 +237,23 @@ export const AdCard = styled.div`
         font-size: 1.2rem;
         color: gold;
     }
+
+    @media (max-width: 768px) {
+        width: 100%;
+        height: auto;
+        margin-top: 1rem;
+
+        img {
+            width: 100%;
+            height: auto;
+        }
+    }
 `;
 
 export const ProfileAnuncio = styled.div`
     position: relative;
 
-    img{
+    img {
         width: 50px;
         height: 50px;
         border-radius: 50%;
@@ -186,7 +262,14 @@ export const ProfileAnuncio = styled.div`
         left: 50%;
         transform: translate(-50%, -50%);
         z-index: 1;
-}
+    }
+
+    @media (max-width: 768px) {
+        img {
+            width: 40px;
+            height: 40px;
+        }
+    }
 `;
 
 export const Footer = styled.footer`
@@ -200,7 +283,17 @@ export const Footer = styled.footer`
         padding: 2rem;
 
         p {
-        margin-right: 55px;
+            margin-right: 55px;
+        }
+    }
+
+    @media (max-width: 768px) {
+        flex-direction: column;
+        align-items: center;
+
+        div {
+            padding: 1rem;
+            text-align: center;
         }
     }
 `;
