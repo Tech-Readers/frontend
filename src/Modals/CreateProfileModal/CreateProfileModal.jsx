eec94ebc6291493dfec193836f641190fad5acbf
FrancisLauriano <130801505+FrancisLauriano@users.noreply.github.com>
2024-09-04 12:57:50 -0300

    atualizado

---
+++ src/Modals/CreateProfileModal/CreateProfileModal.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ModalContainer, ModalContent, Icon, Message, ButtonGroup, StyledButton } from './CreateProfileModal.styles';
+import successIcon from '../../assets/success-icon.svg'; // Caminho para o ícone de sucesso
+
+const CreateProfileModal = ({ onConfirm }) => {
+  return (
+    <ModalContainer>
+      <ModalContent>
+        <Icon>
+          <img src={successIcon} alt="Sucesso" />
+        </Icon>
+        <Message>Conta criada com sucesso</Message>
+        <ButtonGroup>
+          <StyledButton onClick={onConfirm}>OK</StyledButton> 
+        </ButtonGroup>
+      </ModalContent>
+    </ModalContainer>
+  );
+};
+
+export default CreateProfileModal;
